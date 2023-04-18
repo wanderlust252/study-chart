9154d8cce989286beff0016b3317e51ca0e79d49
wanderlust252 <wanderlust252@gmail.com>
2023-04-18 21:17:58 +0700

    ci gogo

--- src/components/HomeHeader/HomeHeader.tsx
+++ src/components/HomeHeader/HomeHeader.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Alert, Platform, Text, TouchableOpacity } from 'react-native'
 import { Container, HeaderTitle } from './HomeHeader.styled'
-
+import Crashes from 'appcenter-crashes'
+import Analytics from 'appcenter-analytics'
 export const HomeHeader = () => {
+  useEffect(() => {
+    const checkCrash = async () => {
+      const didCrash = await Crashes.hasCrashedInLastSession()
+      if (didCrash) {
+        Alert.alert('Report', 'The application had been crashed. We are working to fix it!')
+      }
+    }
+    checkCrash()
+  }, [])
   return (
     <Container>
       <HeaderTitle>Charts Library</HeaderTitle>
+      <TouchableOpacity
+        onPress={() => {
+          // Crashes.generateTestCrash()
+          Analytics.trackEvent('Tracking', {
+            platform: Platform.OS,
+            version: Platform.Version + '',
+          })
+          // Analytics.trackEvent('ABC')
+        }}>
+        <Text>ABC</Text>
+      </TouchableOpacity>
     </Container>
   )
 }
